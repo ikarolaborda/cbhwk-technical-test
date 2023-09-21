39e97b7345c1f4bd12179f2cb799c215d65f6cb3
Ikaro C. Laborda <iclaborda@msn.com>
2023-09-20 21:20:55 -0300

    feat: implementing entities

---
+++ app/Models/Turbine.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Turbine extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+        'name' => 'string',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public function components(): HasMany
+    {
+        return $this->hasMany(TurbineComponent::class);
+    }
+}
